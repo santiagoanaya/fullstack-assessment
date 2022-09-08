04d82d0cd421fce9f8858a96b3b4e588aee0d0fa
Santiago Anaya <anayasantiagohernan@gmail.com>
2022-09-08 18:47:53 -0300

    Add seeds and minor fixes in form

--- back/db/seeds.rb
+++ back/db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Seeding..."
+
+  condiments = Answer.create([{title: 'Ketchup'}, {title: 'Mostaza'}, {title: 'Mayonesa'}])
+  food = Answer.create([{title: 'Ensalada'}, {title: 'Arroz'}, {title: 'Pasta'}])
+  activities = Answer.create([{title: 'Caminar'}, {title: 'Correr'}, {title: 'Dormir'}])
+  moments = Answer.create([{title: 'Mañana'}, {title: 'Tarde'}, {title: 'Noche'}])
+  animals = Answer.create([{title: 'Delfin'}, {title: 'Perro'}, {title: 'Gato'}])
+  stop_eating = Answer.create([{title: 'Carne'}, {title: 'Leche'}, {title: 'Huevos'}])
+
+  food_questions = Question.create([{title: 'Cual es tu comida favorita', question_type: 0, answers: food}, {title: 'Que condimentos te gustan', question_type: 1, answers: condiments}, {title: 'Describe tu lugar favorito para comer', question_type: 2}])
+  life_questions = Question.create([{title: 'Cuales son tus momentos favoritos del dia', question_type: 1, answers: moments}, {title: 'Cual es tu actividad favorita', question_type: 0, answers: activities}, {title: 'Describe un dia ideal', question_type: 2}])
+  animal_questions = Question.create([{title: 'Que opinas sobre los animales', question_type: 2}, {title: 'Cual es tu animal favorito', question_type: 0, answers: animals}, {title: 'Que alimentos dejaste de consumir', question_type: 1, answers: stop_eating}])
+
+  surveys = Survey.create([{title: 'Encuesta culinaria', description: 'Una encuesta sobre comidas y condimentos para deleitarse', questions: food_questions}, {title: 'Encuesta sobre la vida', description: 'Una encuesta para saber sobre uno', questions: life_questions}, {title: 'Encuesta de animales', description: 'Que tanto sabes y conoces a los animales de este planeta', questions: animal_questions},])
+
+puts "Seeding done."
--- frontend/components/SurveyForm.vue
+++ frontend/components/SurveyForm.vue
@@ -3,169 +3,177 @@
     <div class="py-10 accent-bg px-6 d-flex flex-column align-center">
       <h2>Creación de encuesta</h2>
     </div>
-    <validation-observer ref="observer" v-slot="{ invalid }">
-      <form @submit.prevent="submit">
-        <v-container fluid>
-          <v-row>
-            <v-col cols="3">
-              <v-subheader>Título</v-subheader>
-            </v-col>
-            <v-col cols="8">
-              <v-text-field
-                v-model="creationSurvey.title"
-                :counter="40"
-                label="Ingrese un Título"
-                outlined
-                required
-              />
-            </v-col>
-          </v-row>
+    <form @submit.prevent="submit">
+      <v-container fluid>
+        <v-row>
+          <v-col cols="3">
+            <v-subheader>Título</v-subheader>
+          </v-col>
+          <v-col cols="8">
+            <v-text-field
+              v-model="creationSurvey.title"
+              :counter="40"
+              label="Ingrese un Título"
+              outlined
+              required
+            />
+          </v-col>
+        </v-row>
 
-          <v-row>
-            <v-col cols="3">
-              <v-subheader>Descripción</v-subheader>
-            </v-col>
-            <v-col cols="8">
-              <v-text-field
-                v-model="creationSurvey.description"
-                :counter="60"
-                label="Ingrese una Descripción"
-                outlined
-                required
-              />
-            </v-col>
-          </v-row>
+        <v-row>
+          <v-col cols="3">
+            <v-subheader>Descripción</v-subheader>
+          </v-col>
+          <v-col cols="8">
+            <v-text-field
+              v-model="creationSurvey.description"
+              :counter="60"
+              label="Ingrese una Descripción"
+              outlined
+              required
+            />
+          </v-col>
+        </v-row>
 
-          <v-row>
-            <v-col cols="12">
-              <v-row
-                v-for="(question, index) in creationSurvey.questions"
-                :key="index"
-              >
-                <v-col cols="11">
-                  <v-container class="border my-4">
-                    <v-row>
+        <v-row>
+          <v-col cols="12">
+            <v-row
+              v-for="(question, index) in creationSurvey.questions"
+              :key="index"
+            >
+              <v-col cols="11">
+                <v-container class="border my-4">
+                  <v-row>
+                    <v-col cols="3">
+                      <v-subheader>Tipo de Pregunta</v-subheader>
+                    </v-col>
+                    <v-col cols="8">
+                      <v-select
+                        v-model="question.question_type"
+                        label="Elija un Tipo"
+                        :items="answersTypes"
+                        item-text="text"
+                        item-value="value"
+                        data-vv-name="select"
+                        outlined
+                        required
+                      />
+                    </v-col>
+                  </v-row>
+                  <v-row>
+                    <v-col cols="3">
+                      <v-subheader>Pregunta</v-subheader>
+                    </v-col>
+                    <v-col cols="8">
+                      <v-text-field
+                        v-model="question.title"
+                        :counter="40"
+                        label="Pregunta"
+                        outlined
+                        required
+                      />
+                    </v-col>
+                  </v-row>
+
+                  <div v-if="question.question_type !== 'free_choice'">
+                    <v-row
+                      v-for="(answer, index) in question.answers"
+                      :key="index"
+                    >
                       <v-col cols="3">
-                        <v-subheader>Tipo de Pregunta</v-subheader>
+                        <v-subheader
+                          >Opción {{ getAnswerNumber(index) }}</v-subheader
+                        >
                       </v-col>
-                      <v-col cols="8">
-                        <v-select
-                          v-model="question.question_type"
-                          label="Elija un Tipo"
-                          :items="answersTypes"
-                          item-text="text"
-                          item-value="value"
-                          data-vv-name="select"
-                          outlined
-                          required
-                        />
-                      </v-col>
-                    </v-row>
-                    <v-row>
-                      <v-col cols="3">
-                        <v-subheader>Pregunta</v-subheader>
-                      </v-col>
-                      <v-col cols="8">
+                      <v-col cols="9">
                         <v-text-field
-                          v-model="question.title"
-                          :counter="40"
-                          label="Pregunta"
+                          v-model="answer.title"
+                          :label="`Opción ${getAnswerNumber(index)}`"
+                          :rules="[
+                            (text) => !!text || 'This field is required',
+                          ]"
                           outlined
                           required
-                        />
+                        >
+                          <template v-slot:append-outer>
+                            <v-icon
+                              class="ml-2"
+                              color="red"
+                              @click="deleteAnswer(question, index)"
+                              :disabled="question.answers.length < 3"
+                            >
+                              mdi-minus-circle-outline
+                            </v-icon>
+                          </template>
+                        </v-text-field>
                       </v-col>
                     </v-row>
+                    <v-chip class="ma-2" @click="addAnswer(question)">
+                      <v-avatar left>
+                        <v-icon color="purple">mdi-plus-circle</v-icon>
+                      </v-avatar>
+                      Agregar Opción
+                    </v-chip>
+                  </div>
+                </v-container>
+              </v-col>
+              <v-col cols="1" class="align-self-center">
+                <v-icon
+                  color="red"
+                  @click="deleteQuestion(index)"
+                  :disabled="creationSurvey.questions.length < 2"
+                >
+                  mdi-delete
+                </v-icon>
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-row>
 
-                    <div v-if="question.question_type !== 'free_choice'">
-                      <v-row
-                        v-for="(answer, index) in question.answers"
-                        :key="index"
-                      >
-                        <v-col cols="3">
-                          <v-subheader
-                            >Opción {{ getAnswerNumber(index) }}</v-subheader
-                          >
-                        </v-col>
-                        <v-col cols="9">
-                          <v-text-field
-                            v-model="answer.title"
-                            :label="`Opción ${getAnswerNumber(index)}`"
-                            :rules="[
-                              (text) => !!text || 'This field is required',
-                            ]"
-                            outlined
-                            required
-                          >
-                            <template v-slot:append-outer>
-                              <v-icon
-                                class="ml-2"
-                                color="red"
-                                @click="deleteAnswer(question, index)"
-                                :disabled="question.answers.length < 3"
-                              >
-                                mdi-minus-circle-outline
-                              </v-icon>
-                            </template>
-                          </v-text-field>
-                        </v-col>
-                      </v-row>
-                      <v-chip class="ma-2" @click="addAnswer(question)">
-                        <v-avatar left>
-                          <v-icon color="purple">mdi-plus-circle</v-icon>
-                        </v-avatar>
-                        Agregar Opción
-                      </v-chip>
-                    </div>
-                  </v-container>
-                </v-col>
-                <v-col cols="1" class="align-self-center">
-                  <v-icon
-                    color="red"
-                    @click="deleteQuestion(index)"
-                    :disabled="creationSurvey.questions.length < 2"
-                  >
-                    mdi-delete
-                  </v-icon>
-                </v-col>
-              </v-row>
-            </v-col>
-          </v-row>
+        <v-row>
+          <v-col cols="6">
+            <v-chip class="ma-2" @click="addQuestion">
+              <v-avatar left>
+                <v-icon color="purple">mdi-plus-circle</v-icon>
+              </v-avatar>
+              Agregar Pregunta
+            </v-chip>
+          </v-col>
+        </v-row>
 
-          <v-row>
-            <v-col cols="6">
-              <v-chip class="ma-2" @click="addQuestion">
-                <v-avatar left>
-                  <v-icon color="purple">mdi-plus-circle</v-icon>
-                </v-avatar>
-                Agregar Pregunta
-              </v-chip>
-            </v-col>
-          </v-row>
-
-          <v-row>
-            <v-col cols="4">
-              <v-btn @click="$emit('submitSurvey')"> Crear Encuesta </v-btn>
-            </v-col>
-            <!-- <v-col cols="4">
+        <v-row>
+          <v-col cols="4">
+            <v-btn @click="$emit('submitSurvey')"> Crear Encuesta </v-btn>
+          </v-col>
+          <!-- <v-col cols="4">
               <v-btn @click="clear">
                 Limpiar datos
               </v-btn>
             </v-col> -->
-          </v-row>
-        </v-container>
-      </form>
-    </validation-observer>
+        </v-row>
+      </v-container>
+    </form>
   </v-card>
 </template>
 
 <script>
-import { ValidationObserver, ValidationProvider } from "vee-validate"
+const getEmptyAnswer = () => ({
+  title: "",
+})
+const getEmptyQuestion = () => ({
+  question_type: "",
+  title: "",
+  answers: [
+    {
+      title: "",
+    },
+    {
+      title: "",
+    },
+  ],
+})
 
 export default {
-  components: {
-    ValidationProvider,
-    ValidationObserver,
-  },
   props: {
     creationSurvey: {
       type: Object,
@@ -187,36 +195,22 @@ export default {
         value: "free_choice",
       },
     ],
-    newQuestion: {
-      question_type: "",
-      title: "",
-      answers: [
-        {
-          title: "",
-        },
-        {
-          title: "",
-        },
-      ],
-    },
-    newAnswer: {
-      title: "",
-    },
   }),
   methods: {
-    addAnswer(question) {
-      question = question.answers.push(this.newAnswer)
+    addQuestion() {
+      this.creationSurvey.questions.push(getEmptyQuestion())
     },
+    addAnswer(question) {
+      question = question.answers.push(getEmptyAnswer())
+    },
+    // getter in vuex
     deleteAnswer(question, index) {
       question = question.answers.splice(index, 1)
     },
     getAnswerNumber(index) {
       return index + 1
     },
-    // getter in vuex
-    addQuestion() {
-      this.creationSurvey.questions.push(this.newQuestion)
-    },
+
     deleteQuestion(index) {
       if (this.creationSurvey.questions.length < 2) {
         this.creationSurvey.questions.splice(index, 1)
